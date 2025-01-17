0eb60e571589a6c60a99bae003f946532e966272
Mark LaRose <126591378+Mark-LaRose@users.noreply.github.com>
2025-01-16 22:09:25 -0400

    Update Resume.tsx

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -15,7 +15,7 @@ const Resume: React.FC<{ onBack: () => void }> = ({ onBack }) => {
           <img src={resumeImage} alt="My Resume" className="resume-image mx-auto" />
         </div>
         <div className="resume-download-links">
-          <a href={resumeImage} download="Resume.png" className="text-blue-500 block mb-2" target="_blank" rel="noopener noreferrer">Download Resume as PNG</a>
+          <a href={resumeImage} download="Resume.png" className="text-blue-500 block mb-2" target="_blank" rel="noopener noreferrer">Download Resume as PDF</a>
         </div>
         <button className="mt-4 p-2 bg-gray-700 text-white rounded hover:bg-gray-500 hover:text-black" onClick={onBack}>Back to Portfolio</button>
       </div>
